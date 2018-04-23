029d05a6fbc8ada84a40981f8301a14676bd331a
SiobhanGi <siobhan.goggins@gmail.com>
2018-04-23 13:19:35 +0100

    unit and feature test for #top_up, tests pass

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,11 @@ describe Oystercard do
   it 'oystercard has default balance of zero' do
     expect(subject.balance).to eq 0
   end
+
+  describe '#top_up' do
+    it 'respond to top_up' do
+      expect(subject).to respond_to :top_up
+    end
+  end
+
 end
