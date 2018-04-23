a9856e1ebfdb5a5818322f03e47fd5397e7e9664
SiobhanGi <siobhan.goggins@gmail.com>
2018-04-23 12:56:08 +0100

    added attr_reader for balance, renamed balance method to default balance, added initialize method - tests pass

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,10 @@ require 'oystercard'
 
 describe Oystercard do
   it 'check oystercard responds to balance method' do
-    expect(subject).to respond_to :balance
+    expect(subject).to respond_to :default_balance
+  end
+
+  it 'oystercard has default balance of zero' do
+    expect(subject.default_balance).to eq 0
   end
 end
