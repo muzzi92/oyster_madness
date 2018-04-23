413b69fa8d8a9af2a08a233888e61e46e8b982d2
Habin <habin@hotmail.co.uk>
2018-04-23 13:13:45 +0100

    refactored by removing balance method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,10 +2,7 @@ class Oystercard
   attr_reader :balance
 
   def initialize
-    @balance = balance
-  end
-
-  def default_balance
     @balance = 0
   end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,11 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'check oystercard responds to balance method' do
-    expect(subject).to respond_to :default_balance
-  end
 
   it 'oystercard has default balance of zero' do
-    expect(subject.default_balance).to eq 0
+    expect(subject.balance).to eq 0
   end
 end
