8f6693233792e157ce600dce28fac74bd79f964e
Habin <habin@hotmail.co.uk>
2018-04-23 13:57:44 +0100

    fails when balance of 90 is exceeded, added constant for MAX CAPACITY of £90

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,9 +3,11 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @MAX_CAPACITY = 90
   end
 
   def top_up(amount)
+    fail "Balance full!" if (@balance + amount) >= @MAX_CAPACITY
      @balance = @balance + amount
      # @balance
   end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,18 +2,26 @@ require 'oystercard'
 
 describe Oystercard do
 
-  it 'oystercard has default balance of zero' do
+  describe '#balance' do
+    it 'oystercard has default balance of zero' do
     expect(subject.balance).to eq 0
+    end
   end
 
   describe '#top_up' do
     it 'respond to top_up' do
       expect(subject).to respond_to :top_up
     end
+
     it 'adds top_up amount to balance' do
       amount = 5
       expect(subject.top_up(amount)).to eq subject.balance
     end
-  end
 
+    it 'fails when balance of 90 is exceeded' do
+      amount = 91
+      expect{ subject.top_up(amount) }.to raise_error "Balance full!"
+    end
+
+  end
 end
