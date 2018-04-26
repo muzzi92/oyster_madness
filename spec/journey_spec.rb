efcb9a17376264b012996271a62eb5efcf38c23a
Muzzi Aldean <amiraldean@gmail.com>
2018-04-26 13:58:30 +0100

    Debugged journey_spec

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,8 +1,8 @@
-require_relative 'journey'
+require 'journey'
 
 describe Journey do
   let(:oystercard) {double :oystercard, entry_station: :Bank, zone_start: 1, exit_station: :London_Bridge, zone_end: 2 }
-  let(:station) { :Bank }
+  let(:station) { double :station, name: :Bank }
 
   describe '#journey_list' do
     it 'checks if it returns an empty array' do
@@ -12,6 +12,8 @@ describe Journey do
 
   describe '#in_journey' do
     it 'responds as true when touch in is true' do
+      subject.touch_in(station)
+      entry_station = station.name
       expect(subject.in_journey?).to eq true
     end
     it 'responds as false when touch out' do
@@ -28,7 +30,9 @@ describe Journey do
 
   describe '#add_to_journey_list' do
     it 'expect journey list to eq hash' do
-      expect(subject.add_to_journey_list(oystercard)).to eq [{start: :Bank, zone_start: 1, end: :London_Bridge, zone_end: 2}]
+      subject.touch_in(station)
+      entry_station = station.name
+      expect(subject.add_to_journey_list(oystercard)).to eq [{start: :Bank, end: :London_Bridge}]
     end
   end
 
